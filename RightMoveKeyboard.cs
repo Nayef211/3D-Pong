feedc418d953b7653c679821fe931d15d5a8e4d8
aballsballs <andersen.ball@gmail.com>
2017-03-26 11:01:16 -0400

    Add files via upload

---
+++ RightMoveKeyboard.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightMoveKeyboard : MonoBehaviour {
+	public float movespeedR;
+	void Start(){
+	}
+
+	void Update(){
+	transform.Translate (Input.GetAxis ("Horizontal2")*movespeedR, Input.GetAxis ("Vertical2")*movespeedR, 0);
+	
+	}
+	}
